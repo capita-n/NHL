96a50ce8127fe603dccaad81c2e5176e5fe2f117
nath <nhatty94@gmail.com>
2019-07-16 18:54:40 +0100

    first commit

---
+++ schema.js
@@ -0,0 +1,33 @@
+const axios = require('axios')
+
+const { GraphQLObjectType, GraphQLString, GraphQLList, GraphQLSchema } = require('graphql');
+
+//Article Type
+const ArticleType = new GraphQLObjectType({
+    name: 'Artcle',
+    fields: () => ({
+        author: {type:  GraphQLString},
+        title: {type: GraphQLString},
+        urlToImage: {type: GraphQLString},
+        publishedAt:{type: GraphQLString},
+        content:{type: GraphQLString},
+        description:{type: GraphQLString}
+    })
+})
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields:{
+        _articles:{
+            type: new GraphQLList(ArticleType),
+            resolve(parent, args){
+                return axios.get('https://newsapi.org/v2/top-headlines?country=ng&apiKey=3a0570d80ebc48d095f3b31db738e453')
+                    .then(response => response.data.articles);
+            }
+        }
+    }
+})
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+})
